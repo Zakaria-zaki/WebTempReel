c95bf04e1fe80c92ca60d2e4815447032148a6cd
slimane-achbouq <slimane321@gmail.com>
2023-01-31 22:54:06 +0100

    feat fix btn talking to client

--- client/src/components/SideMenu.vue
+++ client/src/components/SideMenu.vue
@@ -40,7 +40,8 @@
               href="#"
               class="flex flex-row items-center h-10 px-3 rounded-lg"
             >
-              <label class="inline-flex relative items-center mr-5 cursor-pointer">
+
+              <label v-if="userRole == 'Admin'" class="inline-flex relative items-center mr-5 cursor-pointer">
                 <input type="checkbox" value="" class="sr-only peer" checked>
                 <div class="w-11 h-6 bg-gray-200 rounded-full peer dark:bg-gray-700 peer-focus:ring-4 peer-focus:ring-green-300 dark:peer-focus:ring-green-800 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-green-600"></div>
                 <span class="ml-3 text-sm font-medium text-gray-300">Talking to clients ?</span>
@@ -184,7 +185,8 @@ export default {
   data() {
     return {
       connectedClients : [],
-      connectedAdministrators : []
+      connectedAdministrators : [],
+      userRole: null
     }
   },
   components: {
@@ -211,6 +213,7 @@ export default {
     if (this.$store.state.auth.token) {
       this.$socket.emit('connected', this.$store.state.auth.token);
     }
+    this.userRole = this.$store.getters['auth/role'];
   }
 }
 </script>
