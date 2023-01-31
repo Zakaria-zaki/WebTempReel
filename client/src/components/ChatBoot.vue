ffe10feef0c55e7ad3b01cabcb635a7f5f1aad40
Zakaria-zaki <03zaki95@gmail.com>
2023-01-31 22:05:41 +0100

    Delete some unused lines

--- client/src/components/ChatBoot.vue
+++ client/src/components/ChatBoot.vue
@@ -2,8 +2,6 @@
 </template>
 <script>
 export default {
-    methods: {
-    },
     mounted (){
       (function(d, m){
         var kommunicateSettings = {"appId":"3a909ff33ce0deab616101d01dac149ea","popupWidget":true,"automaticChatOpenOnNavigation":true};
@@ -12,7 +10,6 @@ export default {
         var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
         window.kommunicate = m; m._globals = kommunicateSettings;
       })(document, window.kommunicate || {});
-      console.log(document.getElementById('mck-sidebox-ft'));
     },
     data: function(){
     }
